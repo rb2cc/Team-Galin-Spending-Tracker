d0dc0d38cccc5cadbdc44433c8ac1ba890171775
rb2cc <99200851+rb2cc@users.noreply.github.com>
2023-03-23 16:28:56 +0000

    Create unseed.py

---
+++ tracker/management/commands/unseed.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand, CommandError
+from tracker.models import UserManager, User, Expenditure, Notification
+
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+
+        User.objects.filter(is_superuser=False).delete()
+        Expenditure.objects.all().delete()
+        Notification.objects.all().delete()
