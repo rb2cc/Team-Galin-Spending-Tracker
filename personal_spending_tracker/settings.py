771adeae78e86e405d078dda7dd2649011f7029f
Reuben Atendido <reuben.atendido@kcl.ac.uk>
2023-03-24 09:01:02 +0000

    fix staticfiles_dirs

--- personal_spending_tracker/settings.py
+++ personal_spending_tracker/settings.py
@@ -128,9 +128,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, 'static'),
-    # os.path.join(BASE_DIR, 'static', 'avatar', 'template')
-    'static/'
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static', 'avatar', 'template')
 ]
 
 # Default primary key field type
